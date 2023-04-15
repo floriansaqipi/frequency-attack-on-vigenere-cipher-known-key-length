2c4a1521bca8bd9fb648db1297d9972ea95404d2
Genci Berisha <gencii.berisha3@gmail.com>
2023-04-15 17:14:36 +0200

    Implemented initializeSortedCharFrequencyMaps(), and sorting result hashmap methods

--- src/main/java/com/example/decrypter/util/AlphabetMaps.java
+++ src/main/java/com/example/decrypter/util/AlphabetMaps.java
@@ -1,6 +1,6 @@
 package com.example.decrypter.util;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class AlphabetMaps {
 
@@ -10,5 +10,24 @@ public class AlphabetMaps {
             hashMap.put(Alphabet.englishAlphabet.charAt(i),0);
         }
         return hashMap;
+    }
+
+    public static LinkedHashMap<Character, Integer> getValueSortedLinkedHashMap(HashMap<Character,Integer> hashMap){
+        ArrayList<Map.Entry<Character,Integer>> entryList = new ArrayList<>(hashMap.entrySet());
+        sortArrayListwithEntries(entryList);
+        LinkedHashMap<Character,Integer> sortedLinkedHashMap = new LinkedHashMap<>();
+        for(Map.Entry<Character,Integer> entry : entryList){
+            sortedLinkedHashMap.put(entry.getKey(),entry.getValue());
+        }
+        return sortedLinkedHashMap;
+    }
+
+    private static void sortArrayListwithEntries(ArrayList<Map.Entry<Character,Integer>> entryList){
+        entryList.sort(new Comparator<Map.Entry<Character, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
     }
 }
